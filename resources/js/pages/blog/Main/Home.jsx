969e70fd0f3f93bbce4f148e38800446bed4851b
Mourad <mourad777b@gmail.com>
2021-07-14 08:02:58 -0400

    changed scroll debounce to 100ms

--- resources/js/pages/blog/Main/Home.jsx
+++ resources/js/pages/blog/Main/Home.jsx
@@ -204,7 +204,7 @@ const Home = ({ scrollWidth, winSize }) => {
                 if (!requestId) {
                     requestId = requestAnimationFrame(update);
                 }
-            }, 200, {}));
+            }, 100, {}));
 
             update();
 
